d668b224081f4b7a20ad56fa2fd6b5a604e30d07
uğur cengiz <ugur_nobre@hotmail.com>
2022-07-26 18:49:28 +0300

    Add project files.

---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        private MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERILER.ToList();
+            return View(degerler);
+        }
+    }
+}
